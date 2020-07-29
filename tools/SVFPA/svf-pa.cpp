c5b2d8ce6237dcf01b597fd0958fdecc24911948
Daisuke Sato <sato@kmckk.co.jp>
2020-07-29 18:16:28 +0900

    SVFPA: Add a simple program SVFPA to dump all points-to information

---
+++ tools/SVFPA/svf-pa.cpp
@@ -0,0 +1,65 @@
+#include "SVF-FE/LLVMUtil.h"
+#include "Graphs/SVFG.h"
+#include "WPA/Andersen.h"
+#include "SVF-FE/PAGBuilder.h"
+
+using namespace llvm;
+using namespace std;
+using namespace SVF;
+
+static llvm::cl::opt<std::string> InputFilename(cl::Positional,
+        llvm::cl::desc("<input bitcode>"), llvm::cl::init("-"));
+
+/*!
+ * An example to print points-to set of an LLVM value
+ */
+std::string printPts(PointerAnalysis* pta, Value* val)
+{
+    std::string str;
+    raw_string_ostream rawstr(str);
+
+    NodeID pNodeId = pta->getPAG()->getValueNode(val);
+    NodeBS& pts = pta->getPts(pNodeId);
+    for (NodeBS::iterator ii = pts.begin(), ie = pts.end();
+            ii != ie; ii++)
+    {
+        rawstr << " " << *ii << " ";
+        PAGNode* targetObj = pta->getPAG()->getPAGNode(*ii);
+        if(targetObj->hasValue())
+        {
+            rawstr << "(" <<*targetObj->getValue() << ")\t ";
+        }
+    }
+
+    return rawstr.str();
+
+}
+
+int main(int argc, char ** argv)
+{
+    if (auto *opt = static_cast<llvm::cl::opt<bool> *>(
+	llvm::cl::getRegisteredOptions().lookup("stat"))) {
+	bool& statOpt = opt->getValue();
+        statOpt = false;
+    }
+
+    int arg_num = 0;
+    char **arg_value = new char*[argc];
+    std::vector<std::string> moduleNameVec;
+    SVFUtil::processArguments(argc, argv, arg_num, arg_value, moduleNameVec);
+    cl::ParseCommandLineOptions(arg_num, arg_value,
+                                "Whole Program Points-to Analysis\n");
+
+    SVFModule* svfModule = LLVMModuleSet::getLLVMModuleSet()->buildSVFModule(moduleNameVec);
+
+    /// Build Program Assignment Graph (PAG)
+    PAGBuilder builder;
+    PAG *pag = builder.build(svfModule);
+
+    /// Create Andersen's pointer analysis
+    Andersen* ander = AndersenWaveDiff::createAndersenWaveDiff(pag);
+
+    ander->dumpAllPts();
+    
+    return 0;
+}
