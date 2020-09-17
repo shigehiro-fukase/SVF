06b887f36dfa3eb588aabf7346d07029a5c734d2
Shigehiro Fukase <shigehiro.fukase@gmail.com>
2020-09-18 00:10:40 +0900

    SVFPA: Visual Studio project files.

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: F:/work/git/SVF
+# Build directory: F:/work/git/SVF/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("lib")
+subdirs("tools")
+subdirs("Test-Suite")
